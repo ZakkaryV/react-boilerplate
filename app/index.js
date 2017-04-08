aac4263fc52cd735187caad8e7a47d0a0e58bdd5
Zakkary <ZakkaryV@gmail.com>
2017-04-07 18:30:00 -0700

    remove css

--- app/index.js
+++ app/index.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import 'main/main.css';
 
-class App extends Component {
+const TodoItem = (props) => {
+    const { handleClick, todoName } = props;
+        return (
+            <div>
+            <input type="text" placeholder={todoName}></input>
+            <button onClick={() => handleClick()}>X</button>
+            </div>
+        )
+    }
+
+class Todo extends Component {
+    constructor() {
+        super();
+    }
+
     render() {
         return (
             <div>
-            Hello World!
+            <TodoItem />
+            </div>
+        )
+    }
+}
+
+class AddTodo extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            todos: ['Do stuff', 'Benchmark PC', 'more things']
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+// e.targetID
+    handleClick() {
+        console.log('clicked')
+    }
+
+    render() {
+        let display = this.state.todos.map((a) => {
+            return <TodoItem todoName={a} handleClick={this.handleClick} />
+        });
+        return (
+            <div>
+            To Do List
+            {display}
             </div>
         )
     }
